d2ba07345b38141cb5d9d138748b453c1cc0e0d4
munenobu.furugen <tpscr11wnp@gmail.com>
2021-06-22 09:39:40 +0000

    login-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-    before_action :require_user_logged_in
+    before_action :require_user_logged_in, only: [:create]
     before_action :set_task, only: [:show, :edit, :update, :destroy]
     before_action :correct_user, only: [:destroy]
     
@@ -18,7 +18,8 @@ class TasksController < ApplicationController
    
     def show
         
-        
+       
+        @tasks = current_user.tasks.order(id: :desc).page(params[:page])
         
     end
    
@@ -68,7 +69,7 @@ class TasksController < ApplicationController
       @task.destroy
      
       flash[:success] = 'Task は正常に削除されました'
-      redirect_to tasks_url
+      redirect_back(fallback_location: root_path)
       
     end
     
@@ -86,7 +87,7 @@ class TasksController < ApplicationController
     end
     
     def correct_user
-      @task = current_user.microposts.find_by(id: params[:id])
+      @task = current_user.tasks.find_by(id: params[:id])
       unless @task
         redirect_to root_url
       end
